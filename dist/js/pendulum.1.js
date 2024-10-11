a886e2ce5fd39baaab38dd740fd1e6a59cd39682
Nafiul555 <nafihaquen5@gmail.com>
2024-10-11 09:27:13 +0600

    updated all

--- dist/js/pendulum.1.js
+++ dist/js/pendulum.1.js
@@ -1,5 +1,6 @@
-import * as THREE from 'three';
-import { OrbitControls } from "https://unpkg.com/three@0.112/examples/jsm/controls/OrbitControls.js";
+
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.112/build/three.module.js';
+import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.112/examples/jsm/controls/OrbitControls.js';
 
 // Scene setup
 const scene = new THREE.Scene();
